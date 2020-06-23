607f256c7b278f1bbc8b53eff8e6fb1d0820f40d
David Whittaker <davidwhittaker@macbook-pro.lan>
2020-06-23 10:24:06 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const ejs = require("ejs");
+const port = 3000;
+
+const homeTitle = "Vern Construction Trainning";
+const trainningTitle = "Trainning";
+const aboutTitle = "About";
+const contactTitle = "Contact";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    pageTitle: homeTitle,
+  });
+});
+
+app.get("/trainning", (req, res) => {
+  res.render("trainning", {
+    pageTitle: trainningTitle,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    pageTitle: aboutTitle,
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    pageTitle: contactTitle,
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server started on port 3000");
+});
