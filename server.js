3b9c6d267bfca1d039d58509fa0070b2fa50f19d
David Whittaker <davidwhittaker@macbook-pro.lan>
2020-06-27 18:27:58 +0100

    update to design

--- server.js
+++ server.js
@@ -3,16 +3,10 @@ const ejs = require("ejs");
 const port = 3000;
 
 // --- Page Title --- //
-const homeTitle = "Vern Construction Training";
+const homeTitle = "";
 const trainingTitle = "Training";
 const aboutTitle = "About";
 const contactTitle = "Contact";
-
-// --- Page Icons --- //
-const homeIcon = "icofont-vehicle-crane";
-const trainingIcon = "icofont-vehicle-excavator";
-const aboutIcon = "icofont-engineer";
-const contactIcon = "icofont-phone";
 
 const app = express();
 
@@ -23,28 +17,24 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("home", {
     pageTitle: homeTitle,
-    pageIcon: homeIcon,
   });
 });
 
-app.get("/trainning", (req, res) => {
-  res.render("trainning", {
+app.get("/training", (req, res) => {
+  res.render("training", {
     pageTitle: trainingTitle,
-    pageIcon: trainingIcon,
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     pageTitle: aboutTitle,
-    pageIcon: aboutIcon,
   });
 });
 
 app.get("/contact", (req, res) => {
   res.render("contact", {
     pageTitle: contactTitle,
-    pageIcon: contactIcon,
   });
 });
 
